030982b74c2347921a7aa1ef3cdadb2c19acb516
JustArchi <JustArchi@JustArchi.net>
2014-11-13 01:50:33 +0100

    EXPERIMENTAL: Let's try to fix Wi-Fi

--- common.mk
+++ common.mk
@@ -67,15 +67,21 @@ PRODUCT_PACKAGES := \
     audio.r_submix.default \
     audio.usb.default \
     com.android.future.usb.accessory \
+    dhcpcd.conf \
     gralloc.exynos4 \
+    hostapd \
+    hostapd_default.conf \
     hwcomposer.exynos4 \
     libfimg \
     libnetcmdiface \
     libsecion \
     libsync \
+    libwpa_client \
     lights.exynos4 \
     macloader \
-    tinymix
+    tinymix \
+    wpa_supplicant \
+    wpa_supplicant.conf
 
 ifneq ($(TARGET_HAS_CAM_FLASH) ,false)
 PRODUCT_PACKAGES += \
