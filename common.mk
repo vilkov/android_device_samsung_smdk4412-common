901d8c7acbcddd5f9b2aff419bb428a04870efe0
tilaksidduram <tilaksidduram@gmail.com>
2014-12-06 19:49:09 +0530

    smdk4412-common: default to awesomeplayer

--- common.mk
+++ common.mk
@@ -176,6 +176,10 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
+# Default to AwesomePlayer
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.use-awesome=true
+
 # Include exynos4 platform specific parts
 TARGET_HAL_PATH := hardware/samsung_slsi/exynos4
 TARGET_OMX_PATH := hardware/samsung/exynos/multimedia/openmax
