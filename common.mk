4244439ecb39d41979d4e8bb06ea4903ddce897e
tilaksidduram <tilaksidduram@gmail.com>
2017-08-26 17:09:31 +0530

    smdk4412: Disable SamsungDoze for now

--- common.mk
+++ common.mk
@@ -105,8 +105,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-PRODUCT_PACKAGES += \
-    SamsungDoze
+#PRODUCT_PACKAGES += \
+#    SamsungDoze
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
